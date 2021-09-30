cd53a10a94e55e5911dbbab67a31193bfc5b0356
Santiago Forero <saforero88@gmail.com>
2021-09-30 11:53:34 -0500

    funcion reset de listas y variables de la busqueda

--- script.js
+++ script.js
@@ -1,14 +1,95 @@
 import { red } from "./capitales_JSON.js";
+let cap_inicio;
+let cap_final;
+let inicioCap = document.getElementById("icap");
+let finalCap = document.getElementById("fcap");
+let formulario = document.getElementById("Capitales");
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(inicioCap.value);
+  console.log(finalCap.value);
+  cap_inicio = inicioCap.value;
+  cap_final = finalCap.value;
+  reset();
+  busqueda();
+});
 
-let cap_inicio = "bogota";
-let cap_final = "medellin";
 let encontrado = false;
 let visitados = new Array();
 let lista = new Array();
 let sinconex = new Array();
 let i = 0;
 
-encontrarNodo(cap_inicio);
+function busqueda() {
+  console.log(cap_final + cap_inicio);
+  encontrarNodo(cap_inicio);
+
+  while (visitados.length != 0 && encontrado == false) {
+    i++;
+    console.log(i);
+    if (
+      visitados[visitados.length - 1].Conexion[0] &&
+      lista.includes(visitados[visitados.length - 1].Conexion[0].capital) ==
+        false
+    ) {
+      encontrarNodo(visitados[visitados.length - 1].Conexion[0].capital);
+      console.log("entro if 1");
+      // console.log(visitados);
+      // console.log(lista);
+    } else if (
+      visitados[visitados.length - 1].Conexion[1] &&
+      lista.includes(visitados[visitados.length - 1].Conexion[1].capital) ==
+        false
+    ) {
+      encontrarNodo(visitados[visitados.length - 1].Conexion[1].capital);
+      console.log("entro if 2");
+      // console.log(visitados);
+      // console.log(lista);
+    } else if (
+      visitados[visitados.length - 1].Conexion[2] &&
+      lista.includes(visitados[visitados.length - 1].Conexion[2].capital) ==
+        false
+    ) {
+      encontrarNodo(visitados[visitados.length - 1].Conexion[2].capital);
+      console.log("entro if 3");
+      // console.log(visitados);
+      // console.log(lista);
+    } else if (
+      visitados[visitados.length - 1].Conexion[3] &&
+      lista.includes(visitados[visitados.length - 1].Conexion[3].capital) ==
+        false
+    ) {
+      encontrarNodo(visitados[visitados.length - 1].Conexion[3].capital);
+      console.log("entro if 4");
+      // console.log(visitados);
+      // console.log(lista);
+    } else if (
+      visitados[visitados.length - 1].Conexion[4] &&
+      lista.includes(visitados[visitados.length - 1].Conexion[4].capital) ==
+        false
+    ) {
+      encontrarNodo(visitados[visitados.length - 1].Conexion[4].capital);
+      console.log("entro if 5");
+      // console.log(visitados);
+      // console.log(lista);
+    } else {
+      console.log(lista);
+      //visitados.pop(visitados[visitados.length - 3]);
+      sinconex.push(visitados[visitados.length - 1]);
+
+      while (sinconex.includes(visitados[visitados.length - 1])) {
+        visitados.pop(visitados[visitados.length - 1]);
+        console.log(sinconex);
+        console.log(visitados);
+      }
+    }
+    if (visitados[visitados.length - 1].Capital == cap_final) {
+      encontrado = true;
+      console.log("yaaaa");
+      console.log(visitados);
+    }
+  }
+}
 
 function encontrarNodo(referencia) {
   for (let i = 0; i < red.length; i++) {
@@ -20,68 +101,12 @@ function encontrarNodo(referencia) {
     }
   }
 }
-
-while (visitados.length != 0 && encontrado == false) {
-  i++;
-  console.log(i);
-  if (
-    visitados[visitados.length - 1].Conexion[0] &&
-    lista.includes(visitados[visitados.length - 1].Conexion[0].capital) == false
-  ) {
-    encontrarNodo(visitados[visitados.length - 1].Conexion[0].capital);
-    console.log("entro if 1");
-    // console.log(visitados);
-    // console.log(lista);
-  } else if (
-    visitados[visitados.length - 1].Conexion[1] &&
-    lista.includes(visitados[visitados.length - 1].Conexion[1].capital) == false
-  ) {
-    encontrarNodo(visitados[visitados.length - 1].Conexion[1].capital);
-    console.log("entro if 2");
-    // console.log(visitados);
-    // console.log(lista);
-  } else if (
-    visitados[visitados.length - 1].Conexion[2] &&
-    lista.includes(visitados[visitados.length - 1].Conexion[2].capital) == false
-  ) {
-    encontrarNodo(visitados[visitados.length - 1].Conexion[2].capital);
-    console.log("entro if 3");
-    // console.log(visitados);
-    // console.log(lista);
-  } else if (
-    visitados[visitados.length - 1].Conexion[3] &&
-    lista.includes(visitados[visitados.length - 1].Conexion[3].capital) == false
-  ) {
-    encontrarNodo(visitados[visitados.length - 1].Conexion[3].capital);
-    console.log("entro if 4");
-    // console.log(visitados);
-    // console.log(lista);
-  } else if (
-    visitados[visitados.length - 1].Conexion[4] &&
-    lista.includes(visitados[visitados.length - 1].Conexion[4].capital) == false
-  ) {
-    encontrarNodo(visitados[visitados.length - 1].Conexion[4].capital);
-    console.log("entro if 5");
-    // console.log(visitados);
-    // console.log(lista);
-  } else {
-    
-    console.log(lista);
-    //visitados.pop(visitados[visitados.length - 3]);
-    sinconex.push(visitados[visitados.length - 1]);
-    
-    while(sinconex.includes(visitados[visitados.length - 1])){
-      visitados.pop(visitados[visitados.length - 1])
-      console.log(sinconex);
-      console.log(visitados);
-    }
-    
-    
-  }
-  if (visitados[visitados.length - 1].Capital == cap_final ){
-    encontrado = true;
-  }
-
+function reset() {
+  visitados.splice(0, visitados.length);
+  lista.splice(0, lista.length);
+  sinconex.splice(0, sinconex.length);
+  console.log(visitados);
+  console.log(lista);
+  console.log(sinconex);
+  encontrado = false;
 }
-console.log("yaaaa");
-console.log(visitados);
